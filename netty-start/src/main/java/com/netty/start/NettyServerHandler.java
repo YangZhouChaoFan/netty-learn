9c699d09a207aa92757bcf9268e7aeb7e73b5be9
YangZhouChaoFan <chenhao1000@live.cn>
2016-03-17 17:22:41 +0800

    netty开始体验

--- netty-start/src/main/java/com/netty/start/NettyServerHandler.java
+++ netty-start/src/main/java/com/netty/start/NettyServerHandler.java
@@ -1,6 +1,5 @@
 package com.netty.start;
 
-import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPromise;
