cc7c172f21a224d26561497f0b30aaf5936d52ce
YangZhouChaoFan <chenhao1000@live.cn>
2016-03-17 18:14:04 +0800

    netty开始体验

--- netty-start/src/main/java/com/netty/start/client/ChannelHandler.java
+++ netty-start/src/main/java/com/netty/start/client/ChannelHandler.java
@@ -3,32 +3,25 @@ package com.netty.start.client;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.handler.codec.DelimiterBasedFrameDecoder;
-import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.LineBasedFrameDecoder;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.nio.charset.Charset;
 
 /**
  * 通道处理类.
  */
 public class ChannelHandler extends ChannelInitializer<SocketChannel> {
 
-    static private Logger logger = LoggerFactory.getLogger(ChannelHandler.class);
-
     @Override
     protected void initChannel(SocketChannel socketChannel) throws Exception {
 
-        logger.info("客户端链接到服务端");
         ChannelPipeline pipeline = socketChannel.pipeline();
         // 以("\n")为结尾分割的 解码器
-        pipeline.addLast("framer", new DelimiterBasedFrameDecoder(1024, Delimiters.lineDelimiter()));
+        pipeline.addLast(new LineBasedFrameDecoder(1024));
         // 字符串解码 和 编码
-        pipeline.addLast("decoder", new StringDecoder(Charset.forName("GBK")));
-        pipeline.addLast("encoder", new StringEncoder(Charset.forName("UTF-8")));
+        pipeline.addLast(new StringDecoder());
+        pipeline.addLast(new StringEncoder());
+
         socketChannel.pipeline().addLast(new NettyClientHandler());
 
     }
--- netty-start/src/main/java/com/netty/start/server/ChildChannelHandler.java
+++ netty-start/src/main/java/com/netty/start/server/ChildChannelHandler.java
@@ -3,32 +3,24 @@ package com.netty.start.server;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.handler.codec.DelimiterBasedFrameDecoder;
-import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.LineBasedFrameDecoder;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.nio.charset.Charset;
 
 /**
  * 客户端通道处理类.
  */
 public class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
 
-    static private Logger logger = LoggerFactory.getLogger(ChildChannelHandler.class);
-
     @Override
     protected void initChannel(SocketChannel e) throws Exception {
 
-        logger.info(e.remoteAddress() + "：进入通道");
         ChannelPipeline pipeline = e.pipeline();
         // 以("\n")为结尾分割的 解码器
-        pipeline.addLast("framer", new DelimiterBasedFrameDecoder(1024, Delimiters.lineDelimiter()));
+        pipeline.addLast(new LineBasedFrameDecoder(1024));
         // 字符串解码 和 编码
-        pipeline.addLast("decoder", new StringDecoder(Charset.forName("GBK")));
-        pipeline.addLast("encoder", new StringEncoder(Charset.forName("UTF-8")));
+        pipeline.addLast(new StringDecoder());
+        pipeline.addLast(new StringEncoder());
         //添加消息处理
         e.pipeline().addLast(new NettyServerHandler());
 
