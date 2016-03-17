08bb7f6a487897f79b66e0abd71220c24fd85869
YangZhouChaoFan <chenhao1000@live.cn>
2016-03-17 17:12:07 +0800

    netty开始体验

--- netty-start/src/main/java/com/netty/start/ChildChannelHandler.java
+++ netty-start/src/main/java/com/netty/start/ChildChannelHandler.java
@@ -11,6 +11,8 @@ import io.netty.handler.codec.string.StringEncoder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.nio.charset.Charset;
+
 /**
  * 客户端通道处理类.
  */
@@ -22,15 +24,12 @@ public class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
     protected void initChannel(SocketChannel e) throws Exception {
 
         logger.info(e.remoteAddress() + "：进入通道");
-
-
         ChannelPipeline pipeline = e.pipeline();
-
         // 以("\n")为结尾分割的 解码器
         pipeline.addLast("framer", new DelimiterBasedFrameDecoder(1024, Delimiters.lineDelimiter()));
         // 字符串解码 和 编码
-        pipeline.addLast("decoder", new StringDecoder());
-        pipeline.addLast("encoder", new StringEncoder());
+        pipeline.addLast("decoder", new StringDecoder(Charset.forName("GBK")));
+        pipeline.addLast("encoder", new StringEncoder(Charset.forName("UTF-8")));
         //添加消息处理
         e.pipeline().addLast(new NettyServerHandler());
 
