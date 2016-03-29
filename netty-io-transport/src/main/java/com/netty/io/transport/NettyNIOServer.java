0dba24c3486acd5dee46d375c6d1b3027e1275a8
YangZhouChaoFan <chenhao1000@live.cn>
2016-03-29 14:17:05 +0800

    netty-io-transport

--- netty-io-transport/src/main/java/com/netty/io/transport/NettyNIOServer.java
+++ netty-io-transport/src/main/java/com/netty/io/transport/NettyNIOServer.java
@@ -20,32 +20,32 @@ public class NettyNIOServer {
 
     public void server(int port) throws Exception {
         final ByteBuf buf = Unpooled.unreleasableBuffer(Unpooled.copiedBuffer("Hi!\r\n", CharsetUtil.UTF_8));
-        // 事件循环组
+        //事件循环组
         EventLoopGroup group = new NioEventLoopGroup();
         try {
-            // 用来引导服务器配置
+            //用来引导服务器配置
             ServerBootstrap b = new ServerBootstrap();
-            // 使用NIO异步模式
+            //使用NIO异步模式
             b.group(group).channel(NioServerSocketChannel.class).localAddress(new InetSocketAddress(port))
-                    // 指定ChannelInitializer初始化handlers
+                    //指定ChannelInitializer初始化handlers
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel ch) throws Exception {
-                            // 添加一个“入站”handler到ChannelPipeline
+                            //添加一个“入站”handler到ChannelPipeline
                             ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
                                 @Override
                                 public void channelActive(ChannelHandlerContext ctx) throws Exception {
-                                    // 连接后，写消息到客户端，写完后便关闭连接
+                                    //连接后，写消息到客户端，写完后便关闭连接
                                     ctx.writeAndFlush(buf.duplicate()).addListener(ChannelFutureListener.CLOSE);
                                 }
                             });
                         }
                     });
-            // 绑定服务器接受连接
+            //绑定服务器接受连接
             ChannelFuture f = b.bind().sync();
             f.channel().closeFuture().sync();
         } catch (Exception e) {
-            // 释放所有资源
+            //释放所有资源
             group.shutdownGracefully();
         }
     }
